5be9c8b1bf1a5f5d96485e2ea87ee45be8905fe5
Ramanan Abeyakaran <65633461+RamAbey@users.noreply.github.com>
2022-11-02 22:40:09 -0700

    Update allocation.py

--- allocation.py
+++ allocation.py
@@ -30,9 +30,8 @@ class MidPoint(Allocation):
             curr_bandit = learned_bandit_arr.get_bandit(user, item)
             price = self.get_price(curr_bandit)
             signal = bandit_arr.get_bandit(user, item).pull_arm(price)
+            print(price, signal)
             curr_bandit.process_signal(signal, price)
-            print(price)
-            print(signal)
             revenue += signal*price
             print(revenue)
             acceptances += signal
