56cd42d4fdcc32d3b174083545dfcb9075d6a6d9
Ramanan Abeyakaran <65633461+RamAbey@users.noreply.github.com>
2022-11-03 00:37:07 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import bandit, simulation
-from allocation import MidPoint, GradLower
+from allocation import MidPoint, GradLower, Walrasian
 from simulation import Simulation
 from pyomo.opt import SolverFactory
 import matplotlib.pyplot as plt
@@ -12,8 +12,12 @@ if __name__ == "__main__":
     num_items = int(input("Num Items? "))
     if allocation_name == "midpoint":
         allocation = MidPoint()
-    if allocation_name == "gradlower":
+    elif allocation_name == "gradlower":
         allocation = GradLower()
+    elif allocation_name == "walrasian":
+        allocation = Walrasian()
+    else:
+        raise Exception("Allocation not implemented.")
     simulation = Simulation(allocation, num_users, num_items, solver=SolverFactory('glpk'))
     revenue = simulation.run_simulation(iterations)
     print("True Optimal: ", simulation.get_optimal())
